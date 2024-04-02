ac2786dda390e447a57c34ebb61a623325369086
reinheimermat <mreinheimer0@gmail.com>
2024-04-01 23:50:33 -0300

    style: add tailwindcss.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import './App.css'
-
-function App() {
-  return <h1>Hello World</h1>
+export default function App() {
+  return <h1 className="text-cyan-300">Hello World</h1>
 }
-
-export default App
